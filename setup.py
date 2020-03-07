f8e5c90fba5e8c620beed9194179e0042ca87ab3
Erik Tuerke <etuerke@googlemail.com>
2020-03-07 11:07:55 +0100

    add setup classifiers

--- setup.py
+++ setup.py
@@ -5,5 +5,25 @@ from restit import __version__
 setup(
     name="restit",
     version=__version__,
-    packages=find_packages()
+    packages=find_packages(),
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
 )
