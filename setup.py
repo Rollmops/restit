f0f8d2af98157a0c2220ebde30e25cf7cdb5e3f6
Tuerke Erik <erik.tuerke@bmw.de>
2020-03-12 10:52:15 +0100

    add swagger ui resources

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     name="restit",
     version=__version__,
     packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         "marshmallow",
         "werkzeug"
