bf6e82517ca098d1e62f8fd36bf1f4ff281a1995
Tuerke Erik <erik.tuerke@bmw.de>
2020-03-24 13:44:25 +0100

    do show not expected 200 status in open api resource

--- restit/resource.py
+++ restit/resource.py
@@ -130,11 +130,12 @@ class Resource:
     @staticmethod
     def _find_response_schema_by_status(status: int, method_object: object) -> Union[None, ResponseStatusParameter]:
         response_status_parameters = get_response_status_parameters_for_method(method_object)
-        for response_status_parameter in response_status_parameters:  # type: ResponseStatusParameter
-            if response_status_parameter.status == status:
-                return response_status_parameter
+        if response_status_parameters:
+            for response_status_parameter in response_status_parameters:  # type: ResponseStatusParameter
+                if response_status_parameter.status == status:
+                    return response_status_parameter
 
-        LOGGER.warning("Response status code %d is not expected for %s", status, method_object)
+            LOGGER.warning("Response status code %d is not expected for %s", status, method_object)
 
     @staticmethod
     def _validate_request_body(method_object: object, request: Request) -> Request:
