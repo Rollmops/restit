4258406164cc05c5f6fab756d3017f2784425655
Erik Tuerke <erik.tuerke@bmw.de>
2020-07-03 12:55:13 +0200

    version 0.2.7

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ author = 'Erik Tuerke'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.2.6'
+release = '0.2.7'
 
 # -- General configuration ---------------------------------------------------
 
