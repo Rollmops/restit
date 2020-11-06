c3b05debc2fb283b872d6e382d9281dca7e178fd
Erik Tuerke <etuerke@googlemail.com>
2020-11-06 10:26:12 +0100

    set version to 0.2.9

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ author = 'Erik Tuerke'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.2.7'
+release = '0.2.9'
 
 # -- General configuration ---------------------------------------------------
 
